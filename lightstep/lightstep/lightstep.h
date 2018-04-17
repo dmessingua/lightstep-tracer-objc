9e91330baac64a7b77b92957bfe1deacc1ceac9e
David Messing <dmessing@underarmour.com>
2018-04-17 11:13:26 -0500

    Added Xcode proj

---
+++ lightstep/lightstep/lightstep.h
@@ -0,0 +1,25 @@
+//
+//  lightstep.h
+//  lightstep
+//
+//  Created by David Messing on 4/17/18.
+//  Copyright © 2018 Under Armour, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for lightstep.
+FOUNDATION_EXPORT double lightstepVersionNumber;
+
+//! Project version string for lightstep.
+FOUNDATION_EXPORT const unsigned char lightstepVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <lightstep/PublicHeader.h>
+#import <lightstep/LSClockState.h>
+#import <lightstep/LSSpan.h>
+#import <lightstep/LSSpanContext.h>
+#import <lightstep/LSTracer.h>
+#import <lightstep/LSUtil.h>
+#import <lightstep/LSVersion.h>
+
+
